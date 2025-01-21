1a0cedc2923a96d033b90fb1c987535b8b546278
hrithik-hg <hrithikgupta.hg.1@gmail.com>
2025-01-21 14:15:59 +0530

    create header component

--- src/App.jsx
+++ src/App.jsx
@@ -5,10 +5,12 @@ import About from "./pages/About";
 import Profile from "./pages/Profile";
 import SignUp from "./pages/SignUp";
 import SingIn from "./pages/SingIn";
+import Header from "./components/Header";
 
 function App() {
   return (
     <BrowserRouter>
+    <Header/>
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/about" element={<About />} />

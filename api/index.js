6346d1ff3a3146f294b02aaebdec5e3916e31e12
hrithik-hg <hrithikgupta.hg.1@gmail.com>
2025-01-21 15:47:08 +0530

    create a test api route

--- api/index.js
+++ api/index.js
@@ -1,16 +1,28 @@
 import express from "express";
 import mongoose from "mongoose";
 import dotenv from "dotenv";
-
+import userRoute from './routes/user.route.js'
 dotenv.config();
+
+const app = express();
+const PORT= process.env.PORT || 5000;
+
+
+
 mongoose
   .connect(process.env.MONGO_URI)
   .then(() => {
     console.log("Connected to MongoDB");
-    app.listen(console.log("Server is running on 3000"));
   })
   .catch((err) => {
     console.error("Failed to connect to MongoDB", err);
   });
 
-const app = express();
+
+  app.use('/api/user', userRoute);
+
+  app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`);
+  });
+
+
--- api/models/userModel.js
+++ api/models/userModel.js
@@ -1,25 +1,25 @@
-import mongoose, { model }  from "mongoose";
+import mongoose, { model } from "mongoose";
 
-const userSchema = new mongoose.Schema({
+const userSchema = new mongoose.Schema(
+  {
     username: {
-        type: String,
-        required: true,
-        unique: true,
+      type: String,
+      required: true,
+      unique: true,
     },
     email: {
-        type: String,
-        required: true,
-        unique: true,
+      type: String,
+      required: true,
+      unique: true,
     },
     password: {
-        type: String,
-        required: true,
+      type: String,
+      required: true,
     },
-
-}, 
-{timestamps: true}
+  },
+  { timestamps: true }
 );
 
-const User = mongoose.model('User', userSchema);
+const User = mongoose.model("User", userSchema);
 
 export default User;

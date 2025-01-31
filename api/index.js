f8cbd3af43197c267ea91bfd849f21d7687f5c8e
hrithik-hg <hrithikgupta.hg.1@gmail.com>
2025-01-31 14:42:50 +0530

    create user updates api

--- api/index.js
+++ api/index.js
@@ -3,6 +3,7 @@ import mongoose from "mongoose";
 import dotenv from "dotenv";
 import userRouter from './routes/user.route.js'
 import authRouter from './routes/auth.route.js'
+import cookieParser from "cookie-parser";
 dotenv.config();
 
 const app = express();
@@ -20,6 +21,7 @@ mongoose
   });
 
   app.use(express.json());
+  app.use(cookieParser());
 
   app.use('/api/user', userRouter);
   app.use('/api/auth', authRouter);

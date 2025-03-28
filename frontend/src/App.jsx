07127b7c4c38bb87ce1e2f434bae5a55200bbe0c
hrithik-hg <hrithikgupta.hg.1@gmail.com>
2025-03-28 15:17:02 +0530

    create search page UI

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -10,6 +10,7 @@ import PrivateRoute from "./components/PrivateRoute";
 import CreateListing from "./pages/CreateListing";
 import Listing from './pages/Listing';
 import UpdateListing from "./pages/UpdateListing";
+import Search from "./pages/Search";
 
 function App() {
   return (
@@ -20,6 +21,7 @@ function App() {
         <Route path="/about" element={<About />} />
         <Route path="/sign-in" element={<SingIn />} />
         <Route path="/sign-up" element={<SignUp />} />
+        <Route path="/search" element={<Search />} />
         <Route path='/listing/:listingId' element={<Listing/>}/>
         <Route element={<PrivateRoute />}>
           <Route path="/profile" element={<Profile />} />
